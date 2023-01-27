9e2ee0cf0d0b60003c77f931f023800d239dbedd
Admin <Admin@TEB127008>
2023-01-27 10:12:01 +0100

    Add project files.

---
+++ uppgift5.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_5._2
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur mycket heltal vill du skriva");
+            int tal = int.Parse(Console.ReadLine());
+            int[] talet = new int[tal];
+
+            for (int i = 0; i < tal; i++)
+            {
+                talet[i]= int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < talet.Length; i++)
+                Console.WriteLine(talet[i]);
+        }
+    }
+}
